0837b4e75301ab0ba96cc46da3a61a14f67f5df7
tanigaki <junya.tanigaki.39@gmail.com>
2020-10-21 16:19:57 +0900

    feat: App.tsx で Calculator を使用

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import Calculator from './components/Calculator'
+//____________________________________________
+//
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.wrapper}>
+      <Calculator />
     </div>
-  );
+  )
 }
-
-export default App;
+//____________________________________________
+//
+const styles = {
+  wrapper: {
+    width: '100%',
+    height: '100vh',
+    background: '#fafafa',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}
+//____________________________________________
+//
+export default App
